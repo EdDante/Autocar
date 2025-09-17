afda2ec2ca2a943921fda520f1aea02ea83df4bb
João Victor Fonseca Assis <32324644@ulife.com.br>
2025-09-17 20:25:02 -0300

    class-veiculo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import modelo.Veiculo;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       // Veiculo carro = new Carro("Toyota", "Corolla", 4);
+       // Veiculo moto = new Moto("Honda", "CB 500", true);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Veiculo> veiculos = new ArrayList<>();
+       // veiculos.add(carro);
+      //  veiculos.add(moto);
+
+        for (Veiculo v : veiculos) {
+            System.out.println(v.exibirInformacoes());
         }
     }
 }
